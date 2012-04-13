343f0d623c1022535c4695ac22cb7a2e3a6a96fb
trevoro <trevoro@joyent.com>
2012-04-13 16:33:32 +0000

    [TOOLS-112] Remove platform_version from /etc/release switch

--- lib/ohai/plugins/solaris2/platform.rb
+++ lib/ohai/plugins/solaris2/platform.rb
@@ -51,9 +51,8 @@ File.open("/etc/release") do |file|
       platform "solaris2"
     when /^\s*(NexentaCore)\s.*$/
       platform "nexentacore"
-    when /^\s*(SmartOS)\s(\d{8}T\d{6}Z)\s.*$/
+    when /^\s*(SmartOS)\s.*$/
       platform "smartos"
-      platform_version $2
     end
   end
 end
